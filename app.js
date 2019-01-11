76dcd170dba0456e7e0a5f7b79d38033e77146af
Jeremy <jeremy@jsrath.com>
2019-01-11 21:44:10 +0100

    Minor formatting

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 app.get('/api', (req, res) => {
   const id = req.query.id;
   const sheet = req.query.sheet || 1;
