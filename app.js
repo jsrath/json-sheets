1215e0d66a6865f07049e7a441730c7e7c6278f6
Jeremy <jeremy@jsrath.com>
2019-12-08 11:36:24 +0100

    feat: improve error page styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5000;
 const request = require('request');
 
 app.use((req, res, next) => {
